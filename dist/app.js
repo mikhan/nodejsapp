bdd51fea9a0ccab7961e338f63fc61894b2f0fa4
Jorge Rojas <mikhan_@hotmail.com>
2021-12-02 15:13:46 -0700

    temp

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,20 @@
-console.clear();
-
 const http = require('http')
-const hostname = '127.0.0.1';
-const port = Number(process.env.PORT ?? '3000');
+const hostname = '127.0.0.1'
+const port = 3000
 
-let count = 0
-const version = '1.0.0'
+let count = 1
 
 const server = http.createServer((req, res) => {
-  count++; 
+  const content = []
+  content.push(`Server running at http://${hostname}:${port}`)
+  content.push(`Request id: ${count++}`)
+  content.push(`Request url: ${req.url}`)
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Count: ${count}\nVersion: ${version}\n`);
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(content.join('\n') + '\n')
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
