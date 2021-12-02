af962b4164cefcc4e9c2eac2a1490326b13f95fb
Jorge Rojas <mikhan_@hotmail.com>
2021-12-02 12:57:44 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+console.clear();
+
+const http = require('http')
+const hostname = '127.0.0.1';
+const port = Number(process.env.PORT ?? '3000');
+
+let count = 0
+const version = '1.0.0'
+
+const server = http.createServer((req, res) => {
+  count++; 
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Count: ${count}\nVersion: ${version}\n`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
